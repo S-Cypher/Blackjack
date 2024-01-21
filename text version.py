e5b2ad7c5ffe62cd8ce121976e0ba007ffe44ce3
Keira Stewart <stewartkeira5@gmail.com>
2024-01-21 13:37:10 -0600

    Laying out the rules and gameplay

--- text version.py
+++ text version.py
@@ -1,2 +1,20 @@
 #Blackjack text version
 
+'''
+Player and dealer are dealt two cards each
+Dealer has one card face-up and the other one faced down
+Hit: draw an additional card to increase the hand value
+Stand: keep the current hand and end the turn
+
+After all players have completed their turns, the dealer reveals the hole card
+The dea;er must hit until their hand totals 17 or higher
+If the dealer busts (exceeds 21), players who haven't busted win
+
+Compare the two hand values
+Players win if their hand is closer to 21 than the dealer's hand without busting
+Players lose if their hand exceeds 21 (busts) or if the dealer's hand is closer to 21
+If the player and dealer have the same hand value, it's a push (tie), and the player's bet is returned
+
+I probably won't do any money bets yet. That would be cool to implement, but that'll have to wait
+
+'''
